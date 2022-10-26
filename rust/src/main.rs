bbea380d3f73a3bd57bcdbbc18235872c2762664
李祖明 <solace.lee@outlook.com>
2022-10-26 17:45:03 +0800

    * rust 调用wasm

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,7 @@ extern crate serde_json;
 use std::fs::File;
 
 fn main() {
-    let f = File::open("RT.json").unwrap();
+    let f = File::open("./src/RT.json").unwrap();
     let v: serde_json::Value = serde_json::from_reader(f).unwrap();
     println!("{:?}", v["column"].as_str().unwrap());
     println!("{:?}", v["row"].as_str().unwrap());
