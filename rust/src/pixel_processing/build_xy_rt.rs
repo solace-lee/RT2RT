ad6b5d1c9beb5287a9c7711a92642eab0e6d0fa9
李祖明 <solace.lee@outlook.com>
2023-01-06 16:36:25 +0800

    * rust 准备处理切面逻辑

--- rust/src/pixel_processing/build_xy_rt.rs
+++ rust/src/pixel_processing/build_xy_rt.rs
@@ -16,7 +16,7 @@ pub fn build_xy_rt(line: Vec<Vec<i32>>, bounds: Bounds) {
     let x_layer_num = (x as f64 / x_layer.round()).ceil();
     let y_layer_num = (y as f64 / y_layer.round()).ceil();
 
-    let result = RTResult {
+    let mut result = RTResult {
         x_rt: vec![vec![0; (y * z) as usize]; x_layer_num as usize],
         y_rt: vec![vec![0; (x * z) as usize]; y_layer_num as usize],
     };
@@ -28,4 +28,21 @@ pub fn build_xy_rt(line: Vec<Vec<i32>>, bounds: Bounds) {
         result.y_rt.len(),
         y_layer.round(),
     );
+
+    for z in 0..line.len() {
+        let layer_coords = &line[z];
+        for line_index in 0..(layer_coords.len() / 3) as usize {
+            let line_x_start = &layer_coords[line_index * 3];
+            let line_x_end = &layer_coords[line_index * 3 + 1];
+            let line_y = &layer_coords[line_index * 3 + 2];
+            // 处理X切面
+            let x_slice_begin_position = ((*line_x_start as f64 / x_layer.round()).ceil()) as isize;
+            let x_slice_end_position = ((*line_x_end as f64 / x_layer.round()).ceil()) as isize;
+
+            for x_slice_layer in x_slice_begin_position..x_slice_end_position {}
+            result.x_rt
+        }
+    }
 }
+
+pub fn generate_mask() {}
