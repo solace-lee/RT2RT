15afeb562b019caf9fd33d99de4e39c10997c6ae
李祖明 <solace.lee@outlook.com>
2023-11-03 09:29:21 +0800

    feat: 计算着色器初试

--- vite/src/page/wgpu/renderer.js
+++ vite/src/page/wgpu/renderer.js
@@ -1,5 +1,5 @@
-import shader from "./shaders.wgsl";
-import TriangleMesh from "./triangle_mesh";
+import raytracer_kernel from "./shaders/raytracer_kernel.wgsl";
+import screen_shader from "./shaders/screen_shader.wgsl";
 import { mat4 } from "gl-matrix";
 
 export class Renderer {
@@ -40,102 +40,166 @@ export class Renderer {
   }
 
   async makePipeline() {
-    this.uniformBuffer = this.device.createBuffer({
-      size: 64 * 3,
-      usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-      // mappedAtCreation:
-    });
-
-    // 创建绑定组布局
-    const bindGroupLayout = this.device.createBindGroupLayout({
-      entries: [
-        {
-          // 条目
-          binding: 0, // 绑定点
-          visibility: GPUShaderStage.VERTEX, // 可见性
-          buffer: {
-            // 缓冲区
-            type: "uniform", // 类型
-          },
-        },
-      ],
-    });
-
-    // 创建绑定组,定义在着色器阶段的使用方式
-    this.bindGroup = this.device.createBindGroup({
-      layout: bindGroupLayout, // 绑定组布局
-      entries: [
-        {
-          binding: 0, // 绑定点
-          resource: {
-            // 资源
-            buffer: this.uniformBuffer,
-          },
-        },
-      ],
-    });
-
-    // 创建管线布局
-    const pipelineLayout = this.device.createPipelineLayout({
-      bindGroupLayouts: [bindGroupLayout], // 绑定组布局
-    });
-
-    // 创建渲染管线
-    this.pipeline = this.device.createRenderPipeline({
-      // 创建渲染管线
-      layout: pipelineLayout, // 管线布局
-      vertex: {
-        // 顶点
-        module: this.device.createShaderModule({
-          code: shader,
-        }),
-        entryPoint: "vs_main",
-        buffers: [this.triangleMesh.bufferLayout],
-      },
-      fragment: {
-        // 片段
-        module: this.device.createShaderModule({
-          code: shader,
-        }),
-        entryPoint: "fs_main",
-        targets: [
+    const ray_tracing_bind_group_layout = this.device.createBindGroupLayout(
+      {
+        entries: [
           {
-            format: this.format,
+            binding: 0,
+            visibility: GPUShaderStage.COMPUTE,
+            storageTexture: {
+              access: 'write-only',
+              format: 'rgba8unorm',
+              viewDimension: '2d'
+            }
+          }
+        ]
+      }
+    )
+
+    this.ray_tracing_bind_group = this.device.createBindGroup(
+      {
+        layout: ray_tracing_bind_group_layout,
+        entries: [
+          {
+            binding: 0,
+            resource: this.color_buffer_view
+          }
+        ]
+      }
+    )
+
+
+    const ray_tracing_pipeline_layout = this.device.createPipelineLayout(
+      {
+        bindGroupLayouts: [ray_tracing_bind_group_layout]
+      }
+    )
+
+    this.ray_tracing_pipeline = this.device.createComputePipeline(
+      {
+        layout: ray_tracing_pipeline_layout,
+
+        compute: {
+          module: this.device.createShaderModule(
+            {
+              code: raytracer_kernel,
+            }
+          ),
+          entryPoint: 'main',
+        }
+      }
+    )
+
+    // screen pipeline
+    const screen_bind_group_layout = this.device.createBindGroupLayout(
+      {
+        entries: [
+          {
+            binding: 0,
+            visibility: GPUShaderStage.FRAGMENT,
+            sampler: {}
           },
-        ],
-      },
-      primitive: {
-        // 图元
-        topology: "triangle-list", // 拓扑
-      },
-    });
+          {
+            binding: 1,
+            visibility: GPUShaderStage.FRAGMENT,
+            texture: {}
+          }
+        ]
+      }
+    )
+
+    this.screen_bind_group = this.device.createBindGroup(
+      {
+        layout: screen_bind_group_layout,
+        entries: [
+          {
+            binding: 0,
+            resource: this.sampler
+          },
+          {
+            binding: 1,
+            resource: this.color_buffer_view
+          }
+        ]
+      }
+    )
+
+    const screen_pipeline_layout = this.device.createPipelineLayout(
+      {
+        bindGroupLayouts: [screen_bind_group_layout]
+      }
+    )
+
+    this.screen_pipeline = this.device.createRenderPipeline(
+      {
+        layout: screen_pipeline_layout,
+
+        vertex: {
+          module: this.device.createShaderModule(
+            {
+              code: screen_shader,
+            }
+          ),
+          entryPoint: 'vert_main',
+        },
+        fragment: {
+          module: this.device.createShaderModule(
+            {
+              code: screen_shader,
+            }
+          ),
+          entryPoint: 'frag_main',
+          targets: [
+            {
+              format: 'bgra8unorm'
+            }
+          ]
+        },
+        primitive: {
+          topology: 'triangle-list'
+        }
+      }
+    )
   }
+
+
 
   // 创建静态资源
   createAssets() {
-    this.triangleMesh = new TriangleMesh(this.device);
+    this.color_buffer = this.device.createTexture(
+      {
+        size: {
+          width: this.canvas.width,
+          height: this.canvas.height
+        },
+        format: 'rgba8unorm',
+        usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.TEXTURE_BINDING
+      }
+    )
+
+    this.color_buffer_view = this.color_buffer.createView();
+
+    const samplerDescriptor = {
+      addressModeU: "repeat",
+      addressModeV: "repeat",
+      magFilter: "linear",
+      minFilter: "nearest",
+      mipmapFilter: "nearest",
+      maxAnisotropy: 1
+    }
+
+    this.sampler = this.device.createSampler(samplerDescriptor);
   }
 
   render = () => {
-    this.t += 0.01;
-    if (this.t > 2.0 * Math.PI) {
-      this.t -= 2.0 * Math.PI;
-    }
-
-    const projection = mat4.create();
-    mat4.perspective(projection, Math.PI / 4, 800 / 600, 0.1, 10);
-
-    const view = mat4.create();
-    mat4.lookAt(view, [-2, 0, 2], [0, 0, 0], [0, 0, 1]);
-
-    const model = mat4.create();
-    mat4.rotate(model, model, this.t, [0, 0, 1]);
-
-    this.device.queue.writeBuffer(this.uniformBuffer, 0, model);
-    this.device.queue.writeBuffer(this.uniformBuffer, 64, view);
-    this.device.queue.writeBuffer(this.uniformBuffer, 128, projection);
-
     const commandEncoder = this.device.createCommandEncoder(); // 创建命令编码器
+
+    const ray_trace_pass = commandEncoder.beginComputePass()
+    ray_trace_pass.setPipeline(this.ray_tracing_pipeline)
+    ray_trace_pass.setBindGroup(0, this.ray_tracing_bind_group)
+    ray_trace_pass.dispatchWorkgroups(this.canvas.width, this.canvas.height, 1)
+    ray_trace_pass.end()
+
 
     const renderPass = commandEncoder.beginRenderPass({
       // 渲染通道描述符
@@ -152,11 +216,11 @@ export class Renderer {
     // `setPipeline`方法用于设置渲染通道中使用的管线。管线定义了渲染操作的状态和行为，包括着色器、颜色混合、剔除等¹。
     // `setBindGroup`方法用于设置渲染通道中使用的资源绑定组。资源绑定组定义了一组要绑定在一起的资源以及这些资源在着色器阶段中的使用方式¹。
 
-    renderPass.setPipeline(this.pipeline); // 设置管线
-    renderPass.setBindGroup(0, this.bindGroup); // 设置绑定组
-    renderPass.setVertexBuffer(0, this.triangleMesh.buffer);
-    // renderPass.draw(3, 1, 0); // 绘制
-    renderPass.draw(3); // 绘制
+    renderPass.setPipeline(this.screen_pipeline); // 设置管线
+    renderPass.setBindGroup(0, this.screen_bind_group); // 设置绑定组
+    // renderPass.setVertexBuffer(0, this.triangleMesh.buffer);
+    renderPass.draw(6, 1, 0, 0); // 绘制
+    // renderPass.draw(3); // 绘制
     renderPass.end(); // 结束
     this.device.queue.submit([commandEncoder.finish()]); // 提交
 
